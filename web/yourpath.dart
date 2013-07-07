cebe642254ac2b25aae787d7bbee981bc30952e5
Rodolfo Ferreira <rodolfo@rodolfoferreira.com.br>
2013-07-07 17:49:06 -0300

    initial commit

---
+++ web/yourpath.dart
@@ -0,0 +1,58 @@
+import 'dart:html';
+
+InputElement txtPath = query('#txtPath');
+Element placeHolder = query('#entries .placeholder');
+
+void main() {
+  txtPath
+    ..onPaste.listen(
+        (e) => handleInput(e.clipboardData.getData('Text'))
+    )
+    ..onInput.listen(
+        (e) => handleInput(e.target.value)
+    );
+}
+
+void showError(String message) {
+  DivElement div = query('.alert-box');
+  div.text = message;
+  div.style.display = 'block';
+}
+
+void handleInput(String value) {
+  clearEntries();
+  List<String> entries = value.split(new RegExp(";"));
+  
+  placeHolder.hidden = entries.isNotEmpty;
+  
+  Iterator it = entries.iterator;
+  while(it.moveNext()) {
+    String entry = it.current;
+    if( entry.isNotEmpty )
+      addEntry(entry);
+  }
+}
+
+void addEntry(String entry) {
+  TableElement entries = query("#entries table");
+  var tbody = entries.tBodies.first;
+  TableRowElement newEntry = new Element.tag('tr');
+  
+  var cell = new Element.tag('td');
+  cell.nodes.add(new CheckboxInputElement());
+  newEntry.nodes.add(cell);
+  
+  cell = new Element.tag('td');
+  var input = new TextInputElement();
+  input.value = entry.trim();
+  cell.nodes.add(input);
+  newEntry.nodes.add(cell);
+  
+  tbody.nodes.add(newEntry);
+}
+
+void clearEntries() {
+  TableElement entries = query("#entries table");
+  TableSectionElement tbody = entries.tBodies.first;
+  tbody.children.clear();
+}
